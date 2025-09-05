2c06e0791b39e427a450027e71152ed27732e57c
Vitorpro99 <vitormateusilva4@gmail.com>
2025-09-05 10:40:57 -0300

    Teste

--- controllers/veiculo.controller.js
+++ controllers/veiculo.controller.js
@@ -27,7 +27,7 @@ exports.create = (req,res) =>{
         })
         .catch((err)=>{
             res.status(500).send({
-                message: err.message || "Erro durante a criação de Produto"
+                message: err.message || "Erro durante a criação de Veiculo"
             });
         });
 };
