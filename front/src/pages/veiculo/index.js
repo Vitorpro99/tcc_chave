791c6eb237cb5951a9e1d2c66f9763884b7c0e20
Vitorpro99 <vitormateusilva4@gmail.com>
2025-09-29 01:44:04 -0300

    Enhance vehicle form UI and add placa image
    Adicionado novo fundo para ser usado no campo de entrada da placa. Melhorado o formulário de registro de veículos com rótulos, validação de entrada e novos estilos para melhorar a usabilidade e a aparência. Atualizado o CSS para suportar os novos elementos do formulário e aprimoramentos visuais.

--- front/src/pages/veiculo/index.js
+++ front/src/pages/veiculo/index.js
@@ -5,14 +5,27 @@ export default function VeiculoPage() {
         <div className={styles.body}>
             <div  className={styles.formDiv}>
                 <form className ={styles.form}>
+                    <h1 className={styles.title}>CADASTRO DE VEÍCULOS</h1>
+                    {/* Marca */}
+                    <label className={styles.label} htmlFor="marca">Marca</label>
                     <input className={styles.input} name="marca" type="text" placeholder="Digite a merca do veículo"/>
-                    <input className={styles.input} name="moedelo" type="text" placeholder="Digite o modelo do veículo" /> 
-                    <input className={styles.input} name="ano" type="number" placeholder="Ano do veículo"/> 
-                    <input className={styles.input} name="placa" type="text" placeholder="Digite a placa do veículo" /> 
+                    {/* Modelo */}
+                    <label className={styles.label} htmlFor="modelo">Modelo</label>
+                    <input className={styles.input} name="modelo" type="text" placeholder="Digite o modelo do veículo" /> 
+                    {/* Ano */}
+                    <label className={styles.label} htmlFor="ano">Ano</label>
+                    <input className={styles.input} min="1900" max={2100} name="ano" type="number" placeholder="Ano do veículo"/> 
+                    {/* Placa */}
+                    <label className={styles.label} htmlFor="placa">Placa</label>
+                    <input className={styles.inputPlaca} name="placa" type="text" placeholder="" /> 
+                    {/* Data aquisição */}
                     <label className={styles.label} htmlFor="dataAquisicao">Data de aquisição</label>
-                    <input name="dataAquisicao" type="date"/> 
+                    <input className={styles.input} name="dataAquisicao" type="date"/> 
+                    {/* Setor */}
+                    <label className={styles.label} htmlFor="setorId">Setor</label>
                     <input className={styles.input} name="setorId" placeholder="Digite o setor do veículo" type="number"/> 
-                    <button type="submit">Buscar</button>
+                    
+                    <button className={styles.mainButton} type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
