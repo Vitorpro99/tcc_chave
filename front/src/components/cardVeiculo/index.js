ba768d03cf9080ec73ee4c8248c8ec1c87ebcf97
Vitorpro99 <vitormateusilva4@gmail.com>
2025-10-10 08:11:45 -0300

    pequenas correções

--- front/src/components/cardVeiculo/index.js
+++ front/src/components/cardVeiculo/index.js
@@ -3,11 +3,14 @@ import styles from './cardVeiculo.module.css'
 export default function cardVeiculo({veiculo}) {
 
     return(
-    <div className={styles.div}>
-        <p>{veiculo.marca}</p><p>{veiculo.modelo}</p><p>{veiculo.ano}</p>
-        <p>{veiculo.placa}</p><p>{veiculo.cor}</p>
-        <button type="button">Editar</button><button type="button">Excluir</button>
-    </div>
+    <tr>
+        <td>{veiculo.marca}</td>
+        <td>{veiculo.modelo}</td>
+        <td>{veiculo.ano}</td>
+        <td>{veiculo.placa}</td>
+        <td>{veiculo.cor}</td>
+        <td><button>Editar</button></td>
+    </tr>
     )
 
 }
--- front/src/pages/listaVeiculos/index.js
+++ front/src/pages/listaVeiculos/index.js
@@ -1,28 +1,42 @@
 import api from "@/services/api";
 import { useEffect, useState } from "react";
 import styles from "../../styles/Lista.module.css"
-import CardVeiculo from "@/components/cardVeiculo"; 
+import CardVeiculo from "@/components/cardVeiculo";
 export default function ListaVeiculosPage() {
     const [veiculos, setVeiculos] = useState([])
-    const getVeiculos = () =>{
+    const getVeiculos = () => {
         api
             .get('veiculos')
-            .then((result)=>{
+            .then((result) => {
                 setVeiculos(result.data)
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err);
             })
     }
-    useEffect(()=>{
-        getVeiculos();        
+    useEffect(() => {
+        getVeiculos();
     }, [])
-    
+
     return (
         <>
-            <h3>Listagem de veículos</h3>
             <div className={styles.container}>
-                {veiculos?.length > 0 && veiculos.map((veiculo) => <CardVeiculo veiculo={veiculo}/>)}
+            <h3>Listagem de veículos</h3>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+
+                            <th>Marca</th>
+                            <th>Modelo</th>
+                            <th>Ano</th>
+                            <th>Placa</th>
+                            <th>Cor</th>
+                        </tr>
+                    </thead>
+                    {veiculos?.length > 0 && veiculos.map((veiculo) => <CardVeiculo veiculo={veiculo} />)}
+
+                </table>
+
             </div>
         </>
     )
