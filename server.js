20e4ffea5b9a9c3a882b237f76139e32613c8232
Vitorpro99 <vitormateusilva4@gmail.com>
2025-12-02 19:19:35 -0300

    alter

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 const db = require("./models")
 
 db.sequelize
-    .sync({force:true})
+    .sync({alter:true})
     .then(() =>{
         console.log("Ola mundo!");
     })
